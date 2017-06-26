bad1b33b3b36243312d94892850db219c79258d4
伍朝辉 <337883612@qq.com>
2017-06-26 18:42:28 +0800

    通用导航处理

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
 <template>
   <div id="app">
-     <router-view></router-view>
+    <!--顶部导航组件-->
+    <navbar :info="navinfo"></navbar>
+    <router-view keep-alive @setnav="setNav"></router-view>
   </div>
 </template>
 
 <script>
 
+//引入导航组件
+import navbar from '@/components/global/navbar'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return {
+      navinfo: {}   //导航信息
+    }
+  },
+  methods:{
+    //设置导航信息
+    setNav(info){
+      this.navinfo = info;
+    }
+  },
+  components:{ navbar }
 }
 </script>
 
--- src/components/global/NavBar.vue
+++ src/components/global/NavBar.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="m-header is-bg is-fixed">
+    <div v-if="!config.hide" class="m-header is-bg is-fixed">
         <div class="m-header-button is-left">
-            <router-link  v-if="!noback" :to="backlink">{{ backtext }}</router-link>
+            <router-link  v-if="!config.noback" :to="config.backlink">{{ config.backtext }}</router-link>
         </div>    
-        <h1 class="m-header-title" v-text="title"></h1>  
+        <h1 class="m-header-title" v-text="config.title"></h1>  
         <div class="m-header-button is-right">
             <a href="javascript:;" style="display:none;">分享</a>
         </div>
@@ -13,28 +13,32 @@
 <script>
 
     /**
-    *   导航组件所需要的数据对象为：{title:'标题',backtext:'返回按钮的内容',backlink:'返回的路由', noback: 是否隐藏返回按钮}
+    *   导航组件所需要的数据对象为：{title:'标题',backtext:'返回按钮的内容',backlink:'返回的路由', noback: 是否隐藏返回按钮,hide: 是否隐藏导航}
     *   特别声明：
     *       1、当传递的数据对象为null时，将使用默认信息（_defInfo变量）；
     *       2、当传递的数据对象中某值为null时，将使用默认信息，同上。
     */
 
     //默认导航信息
-    var _defInfo = {title:'优码商城', backtext: '〈 返回', backlink: '/', noback: false};
+    var _defInfo = { title:'优码商城', backtext: '〈 返回', backlink: '/', noback: false, hide: false,};
 
     export default{
-        data() {
-            return {
-                title: this.navinfo.title || _defInfo.title,
-                backtext: this.navinfo.backtext || _defInfo.backtext,
-                backlink: this.navinfo.backlink || _defInfo.backlink ,
-                noback: this.navinfo.noback || _defInfo.noback
+        props:{
+            info:{
+                required: true,
+                type: Object,
+                default: _defInfo
             }
         },
-        props:{
-            navinfo:{
-               type: Object,
-               default: _defInfo
+        computed:{
+            config: function(){
+                return {
+                    title: this.info.title || _defInfo.title,
+                    backtext: this.info.backtext || _defInfo.backtext,
+                    backlink: this.info.backlink || _defInfo.backlink,
+                    noback: this.info.noback || _defInfo.noback,
+                    hide: this.info.hide || _defInfo.hide
+                }
             }
         },
         methods: {
