9191ea37fdddb185a0b9d99e1ea3c4aaffd27e77
廖武斌 <liaowubinf@163.com>
2017-06-30 00:19:27 +0800

    用户登陆状态管理

--- src/components/global/NavBar.vue
+++ src/components/global/NavBar.vue
@@ -1,90 +1,110 @@
 <template>
     <div v-if="!config.hide" class="m-header is-bg is-fixed">
         <div class="m-header-button is-left">
-            <router-link  v-if="!config.noback" :to="config.backlink">{{ config.backtext }}</router-link>
-        </div>    
-        <h1 class="m-header-title" v-text="config.title"></h1>  
+            <router-link v-if="!config.noback" :to="config.backlink">{{ config.backtext }}</router-link>
+        </div>
+        <h1 class="m-header-title" v-text="config.title"></h1>
         <div class="m-header-button is-right">
             <a href="javascript:;" style="display:none;">分享</a>
+        </div>
+    
+        <div class="userInfo" v-show="isShowAvatar">
+            <img :src='avatar'>
         </div>
     </div>
 </template>
 
 <script>
+import security from './../../utils/security';
+/**
+*   导航组件所需要的数据对象为：{title:'标题',backtext:'返回按钮的内容',backlink:'返回的路由', noback: 是否隐藏返回按钮,hide: 是否隐藏导航}
+*   特别声明：
+*       1、当传递的数据对象为null时，将使用默认信息（_defInfo变量）；
+*       2、当传递的数据对象中某值为null时，将使用默认信息，同上。
+*/
 
-    /**
-    *   导航组件所需要的数据对象为：{title:'标题',backtext:'返回按钮的内容',backlink:'返回的路由', noback: 是否隐藏返回按钮,hide: 是否隐藏导航}
-    *   特别声明：
-    *       1、当传递的数据对象为null时，将使用默认信息（_defInfo变量）；
-    *       2、当传递的数据对象中某值为null时，将使用默认信息，同上。
-    */
+//默认导航信息
+var _defInfo = { title: '优码商城', backtext: '〈 返回', backlink: '/', noback: false, hide: false, };
 
-    //默认导航信息
-    var _defInfo = { title:'优码商城', backtext: '〈 返回', backlink: '/', noback: false, hide: false,};
-
-    export default{
-        props:{
-            info:{
-                required: true,
-                type: Object,
-                default: _defInfo
-            }
-        },
-        computed:{
-            config: function(){
-                return {
-                    title: this.info.title || _defInfo.title,
-                    backtext: this.info.backtext || _defInfo.backtext,
-                    backlink: this.info.backlink || _defInfo.backlink,
-                    noback: this.info.noback || _defInfo.noback,
-                    hide: this.info.hide || _defInfo.hide
-                }
-            }
-        },
-        methods: {
-            
+export default {
+    props: {
+        info: {
+            required: true,
+            type: Object,
+            default: _defInfo
         }
+    },
+    computed: {
+        config: function () {
+            return {
+                title: this.info.title || _defInfo.title,
+                backtext: this.info.backtext || _defInfo.backtext,
+                backlink: this.info.backlink || _defInfo.backlink,
+                noback: this.info.noback || _defInfo.noback,
+                hide: this.info.hide || _defInfo.hide
+            }
+        }
+    },
+    data() {
+        return {
+            avatar: '/static/icons/unloginUser.ico', //头像图标
+            isShowAvatar: false // 是否显示头像
+        }
+    },
+    methods: {
+
+    },
+    watch: {
+        "info": {
+            handler(newVal, oldVal) {
+                let user = security.getCurrentUser();
+                this.avatar = user ? '/static/icons/loginUser.ico' : '/static/icons/unloginUser.ico';
+                this.isShowAvatar = (newVal.title != '用户登录' && newVal.title != '新用户注册');
+            }
+
+        }
+
     }
+}
 </script>
 
 <style scoped lang="less">
-
 //APP默认颜色
-@defaultColor:#42bd56;
+@defaultColor: #42bd56;
 //导航底色
-@headerBg:@defaultColor;
+@headerBg: @defaultColor;
 //导航默认前景色
-@headerDefaultColor:rgb(73,73,73);
+@headerDefaultColor: rgb(73, 73, 73);
 
-.m-header{
+.m-header {
     display: flex;
     align-items: center;
     height: 44px;
     padding: 0 10px;
     background: @headerBg;
     color: @headerDefaultColor;
-    border-bottom:1px solid #eee;
-    a{
+    border-bottom: 1px solid #eee;
+    a {
         color: @headerDefaultColor;
     }
-    .m-header-button{
+    .m-header-button {
         width: 70px;
-        align-items:stretch;
-        &.is-left{
+        align-items: stretch;
+        &.is-left {
             text-align: left;
         }
-        &.is-right{
+        &.is-right {
             text-align: right;
         }
-        .m-icon-img{
+        .m-icon-img {
             width: 20px;
             height: 20px;
         }
-        .margin-right-10{
+        .margin-right-10 {
             margin-right: 10px;
         }
     }
-    .m-header-title{
+    .m-header-title {
         flex: 1;
         text-align: center;
         font-size: 16px;
@@ -92,20 +112,26 @@
         white-space: nowrap;
         overflow: hidden;
     }
-    &.is-bg{
+    &.is-bg {
         background: @headerBg;
         color: #fff;
-        a{color: #fff;}
-        .m-header-title{
+        a {
+            color: #fff;
+        }
+        .m-header-title {
             color: #fff;
         }
     }
-    &.is-fixed{
+    &.is-fixed {
         position: fixed;
         left: 0;
         right: 0;
         top: 0;
         z-index: 9;
+    }
+    .userInfo {
+        width: 30px;
+        height: 30px;
     }
 }
 </style>
---
+++ src/utils/security.js
@@ -0,0 +1,57 @@
+// 当前登录用户的Cookie键
+const loginUserCookieKey = '__youma.loginuser_';
+// Cookie过期时间
+const maxage = 30 * 60;
+
+// 是否为json对象
+let isJSON = function(obj) {
+    return typeof obj === "object" && Object.prototype.toString.call(obj).toLowerCase() === "[object object]" && !obj.length;
+};
+
+// 序列化
+let serialize = function(value) {
+    return value === undefined || typeof value === "function" ? value + "" : JSON.stringify(value);
+};
+
+// 反序列化
+let deserialize = function(value) {
+    if (typeof value !== "string") {
+        return undefined;
+    }
+
+    try {
+        return JSON.parse(value);
+    } catch (e) {
+        return value || undefined;
+    }
+};
+
+export default {
+    setCurrentUser(user) {
+        console.log(user);
+        if (user && isJSON(user)) {
+            // 设置当前登录用户的Cookie
+            document.cookie = `${loginUserCookieKey}=${serialize(user)}; max-age=${maxage}; `
+        } else if (user && !isJSON(user)) {
+            document.cookie = `${loginUserCookieKey}=${user}; max-age=${maxage}; `
+        }
+    },
+    getCurrentUser() {
+        var arr, reg = new RegExp("(^| )" + loginUserCookieKey + "=([^;]*)(;|$)");
+        if (arr = document.cookie.match(reg)) {
+
+            let userCookie = unescape(arr[2]);
+
+            let user = deserialize(user);
+            if (user) {
+                this.setCurrentUser(user); //重新续约当前登录用户
+
+                return user;
+            }
+
+            return null;
+        } else
+            return null;
+    }
+
+}
